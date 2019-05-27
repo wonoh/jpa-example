5e9bd359001e3c1011a2039f2d3bee34d68cc74e
jiwonoh <wonoh@zuminternet.com>
2019-05-27 10:36:23 +0900

    유효성 검증 테스트

--- src/test/java/com/wonoh/spring/jpa/member/MemberTest.java
+++ src/test/java/com/wonoh/spring/jpa/member/MemberTest.java
@@ -1,6 +1,7 @@
 package com.wonoh.spring.jpa.member;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wonoh.spring.jpa.common.ErrorCode;
 import com.wonoh.spring.jpa.team.Team;
 import com.wonoh.spring.jpa.team.TeamRepository;
 import org.junit.Before;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -44,21 +46,15 @@ public class MemberTest {
     ObjectMapper objectMapper;
 
 
-
-
     @Test
-    public void 멤버등록() throws Exception{
-
-
-
-        Team team = new Team("아스날");
+    public void 멤버등록() throws Exception {
 
         int age = 27;
         String name = "원오";
         String email = "wnjdsdh@naver.com";
         String teamName = "아스날";
 
-        MemberRequestDto dto = new MemberRequestDto(age,name,email,teamName);
+        MemberRequestDto dto = new MemberRequestDto(age, name, email, teamName);
 
         mockMvc.perform(post("/member")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
@@ -73,5 +69,54 @@ public class MemberTest {
                 .andExpect(jsonPath("team").exists());
 
     }
+
+    @Test
+    public void 멤버등록_빈값_유효성검증실패() throws Exception {
+
+        int age = 27;
+        String name = "원오";
+        String email = " ";
+        String teamName = "아스날";
+
+        MemberRequestDto dto = new MemberRequestDto(age, name, email, teamName);
+
+        mockMvc.perform(post("/member")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .content(objectMapper.writeValueAsString(dto))
+        )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("message").exists())
+                .andExpect(jsonPath("code").exists())
+                .andExpect(jsonPath("status").exists())
+                .andExpect(jsonPath("errors").exists());
+
+    }
+
+    @Test
+    public void 멤버등록_이메일양식_유효성검증실패() throws Exception {
+
+        int age = 27;
+        String name = "원오";
+        String email = "wldnjsdh4412naver.com";
+        String teamName = "아스날";
+
+        MemberRequestDto dto = new MemberRequestDto(age, name, email, teamName);
+
+        mockMvc.perform(post("/member")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .content(objectMapper.writeValueAsString(dto))
+        )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("message").exists())
+                .andExpect(jsonPath("code").value(ErrorCode.INPUT_VALUE_INVALID.getCode()))
+                .andExpect(jsonPath("status").value(400))
+                .andExpect(jsonPath("errors").exists());
+
+    }
+
 
 }
