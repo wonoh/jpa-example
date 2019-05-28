647280f65f182f374fcd2ac4980b6f019e87e5d2
jiwonoh <wonoh@zuminternet.com>
2019-05-28 11:45:15 +0900

    testproperties 추가

--- src/test/java/com/wonoh/spring/jpa/member/MemberTest.java
+++ src/test/java/com/wonoh/spring/jpa/member/MemberTest.java
@@ -6,10 +6,13 @@ import com.wonoh.spring.jpa.team.TeamRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,9 +25,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-@Transactional
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
+@TestPropertySource("classpath:application-test.properties")
+@Transactional
 public class MemberTest {
 
     @Autowired
