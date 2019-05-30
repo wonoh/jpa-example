f03da0207ca6467958160f61601d3201474272da
jiwonoh <wonoh@zuminternet.com>
2019-05-30 17:42:07 +0900

    update fightTest because of baseTest creation

--- src/test/java/com/wonoh/spring/jpa/fight/FightTest.java
+++ src/test/java/com/wonoh/spring/jpa/fight/FightTest.java
@@ -1,78 +1,25 @@
 package com.wonoh.spring.jpa.fight;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wonoh.spring.jpa.common.BaseTest;
 import com.wonoh.spring.jpa.member.Member;
-import com.wonoh.spring.jpa.member.MemberRepository;
-import com.wonoh.spring.jpa.member.MemberRequestDto;
-import com.wonoh.spring.jpa.team.TeamRepository;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.junit.Assert.assertThat;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-@ActiveProfiles("test")
-@TestPropertySource("classpath:application-test.properties")
-@Transactional
-public class FightTest {
+
+public class FightTest extends BaseTest {
 
 
-    @Autowired
-    MockMvc mockMvc;
-
-    @Autowired
-    MemberRepository memberRepository;
-
-    @Autowired
-    ObjectMapper objectMapper;
-
-    @Autowired
-    FightRepository fightRepository;
-
-    private Member member;
+    private Member member = super.member;
 
     @Before
-    public void createMember() throws Exception{
-        int age = 27;
-        String name = "지원오";
-        String email = "wldnjsdh4412@naver.com";
-        String teamName = "팀매드";
-        String gender = "남";
-        int weight = 80;
-
-        MemberRequestDto dto = MemberRequestDto.builder()
-                .age(age)
-                .name(name)
-                .email(email)
-                .teamName(teamName)
-                .gender(gender)
-                .weight(weight)
-                .build();
-
-        mockMvc.perform(post("/member")
-                .contentType(MediaType.APPLICATION_JSON_UTF8)
-                .accept(MediaType.APPLICATION_JSON_UTF8)
-                .content(objectMapper.writeValueAsString(dto))
-        );
-        member = memberRepository.findAll().get(0);
+    public void createMember() throws Exception {
+        super.createMember();
     }
 
     @Test
@@ -84,7 +31,7 @@ public class FightTest {
                 .location("강남")
                 .member(member)
                 .price(30000)
-                .versus("김")
+                .versus("김진짜")
                 .build();
 
         Fight savedFight = fightRepository.save(fight);
