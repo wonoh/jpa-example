3c62255c9e9a21029c75a446e2cf33215cbc1bb7
jiwonoh <wonoh@zuminternet.com>
2019-05-30 15:36:51 +0900

    add fight test code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-### JPA practice example
+### JPA practice
 
-[![Build Status](https://travis-ci.org/wonoh/jpa-example.svg?branch=master)](https://travis-ci.org/wonoh/jpa-example)  [![Coverage Status](https://coveralls.io/repos/github/wonoh/jpa-example/badge.svg?branch=master)](https://coveralls.io/github/wonoh/jpa-example?branch=master)
+[![Build Status](https://travis-ci.org/wonoh/jpa-example.svg?branch=master)](https://travis-ci.org/wonoh/jpa-example)     [![Coverage Status](https://coveralls.io/repos/github/wonoh/jpa-example/badge.svg?branch=master)](https://coveralls.io/github/wonoh/jpa-example?branch=master)
 
 
--- src/test/java/com/wonoh/spring/jpa/fight/FightTest.java
+++ src/test/java/com/wonoh/spring/jpa/fight/FightTest.java
@@ -2,9 +2,11 @@ package com.wonoh.spring.jpa.fight;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wonoh.spring.jpa.member.Member;
 import com.wonoh.spring.jpa.member.MemberRepository;
 import com.wonoh.spring.jpa.member.MemberRequestDto;
 import com.wonoh.spring.jpa.team.TeamRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,9 +47,10 @@ public class FightTest {
     @Autowired
     FightRepository fightRepository;
 
-    @Test
-    public void 파이트생성_성공() throws Exception {
+    private Member member;
 
+    @Before
+    public void createMember() throws Exception{
         int age = 27;
         String name = "지원오";
         String email = "wldnjsdh4412@naver.com";
@@ -69,12 +72,17 @@ public class FightTest {
                 .accept(MediaType.APPLICATION_JSON_UTF8)
                 .content(objectMapper.writeValueAsString(dto))
         );
+        member = memberRepository.findAll().get(0);
+    }
+
+    @Test
+    public void 파이트생성_성공() {
 
 
         Fight fight = Fight.builder()
                 .fightDate(LocalDateTime.of(2019,8,25,3,30))
                 .location("강남")
-                .member(memberRepository.findAll().get(0))
+                .member(member)
                 .price(30000)
                 .versus("김")
                 .build();
